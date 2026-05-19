ca7b0b11f6db10d29c46f27008ab9ec6a4948675
Rudi Timmermans <101011119+ruditimmermans@users.noreply.github.com>
2026-05-19 17:50:28 +0200

    Delete app directory

--- app/src/main/java/com/light/lightemail/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.light.lightemail.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val White = Color(0xFFFFFFFF)
-val Black = Color(0xFF000000)
-val DarkGray = Color(0xFF222222)
-val LightGray = Color(0xFFEEEEEE)
